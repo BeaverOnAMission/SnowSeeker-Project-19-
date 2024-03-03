4b68f0382c26bdc4b5005ae1f7c318c70303b72c
BeaverOnAMission <babenko.bohdan01@gmail.com>
2024-03-03 09:21:40 +0200

    Initial Commit

---
+++ SnowSeeker(Project 19)/SnowSeeker_Project_19_App.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeeker_Project_19_App.swift
+//  SnowSeeker(Project 19)
+//
+//  Created by mac on 04.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeeker_Project_19_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
